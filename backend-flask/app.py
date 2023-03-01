a6da95f3fa1e0a54a090e69a422900d7eb3b386a
EnergeticAWS <wilson8wade@gmail.com>
2023-03-01 11:27:22 +0000

    xray is now working

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -36,12 +36,14 @@ provider.add_span_processor(processor)
 # X-ray ~~~~~~~~
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
 
 app = Flask(__name__)
+
+# X-ray ~~~~~~~~
+XRayMiddleware(app, xray_recorder)
+
 # HoneyComb ~~~~~
 # Initialize automatic instrumentation with Flask
-
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
 

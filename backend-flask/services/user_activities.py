076a91f234586884f5bddb2745240a8598d2d9ae
EnergeticAWS <wilson8wade@gmail.com>
2023-05-22 12:42:10 +0000

    T-Shooting user profile

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,14 +1,12 @@
 from lib.db import db
-# from aws_xray_sdk.core import xray_recorder
-
+#from aws_xray_sdk.core import xray_recorder
 class UserActivities:
   def run(user_handle):
-    # try:
+    #try:
     model = {
       'errors': None,
       'data': None
     }
-    
     if user_handle == None or len(user_handle) < 1:
       model['errors'] = ['blank_user_handle']
     else:
@@ -16,17 +14,15 @@ class UserActivities:
       sql = db.template('users','show')
       results = db.query_object_json(sql,{'handle': user_handle})
       model['data'] = results
-
+      #subsegment = xray_recorder.begin_subsegment('mock-data')
+      ## xray ---
+      #dict = {
+      #  "now": now.isoformat(),
+      #  "results-size": len(model['data'])
+      #}
+      #subsegment.put_metadata('key', dict, 'namespace')
+      #xray_recorder.end_subsegment()
+    #finally:  
+    ##  # Close the segment
+    #  xray_recorder.end_subsegment()
     return model
-      # subsegment = xray_recorder.begin_subsegment('mock-data')
-      # xray ---
-      # dict = {
-      #   "now": now.isoformat(),
-      #   "results-size": len(model['data'])
-      # }
-      # subsegment.put_metadata('key', dict, 'namespace')
-      # xray_recorder.end_subsegment()
-    # finally:  
-    #  # Close the segment
-      # xray_recorder.end_subsegment()
-      # print("Reached Finally statement. I should not have reached here.")
